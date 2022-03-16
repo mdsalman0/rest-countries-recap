a0c89fbcaae5486dd260d102baa8ac360fdc7992
mdsalman0 <mdsalman01864346@gmail.com>
2022-03-16 11:58:04 +0600

    first commit

---
+++ js/countries.js
@@ -0,0 +1,24 @@
+const loadCountries = () =>{
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res=> res.json())
+    .then(data => displayCountries(data))
+}
+
+const displayCountries = (countries)=>{
+    console.log(countries[0]);
+    const allCountriesHtml = countries.map(country => getCountris(country));
+    // console.log(allCountriesHtml[0]);
+    const container = document.getElementById('countries');
+    container.innerHTML = allCountriesHtml.join(' ');
+
+}
+
+const getCountris = country =>{
+    return `
+        <div class="country">
+            <h2>${country.name.common}</h2>
+            <img src="${country.flags.png}">
+        </div>
+    `;
+}
+loadCountries()
